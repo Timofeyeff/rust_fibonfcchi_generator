db62627968a05e69343dfea2f860409c4ff2b420
Maksim <maksim@timofeyeff.ru>
2019-07-10 00:31:30 +0700

    add input module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Привет!");
+    println!("Данная программа вычисляет вычисляет числа Фибоначчи");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .except("Не могу прочитать строку");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Пожалуйста введите целое положительное число:");
+                continue;
+            },
+        };
+    }
 }
